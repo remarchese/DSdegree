7840e2f801734fb88c2773832c645ea65a2dda28
Renata Martins Marchese <renatapink@users.noreply.github.com>
2021-06-19 11:05:43 -0300

    Aula6

---
+++ Aula06/exemplo.py
@@ -0,0 +1,9 @@
+lista = [0, 1, 2, 3, 4, 5, 6, 7]
+
+listafinal = []
+
+for i in lista:
+    if i <= 3:
+        listafinal.append(i) # se o elemento for menor igual a 3, jogo para a lista final
+        
+print(listafinal)
