e66dad36c05aa631bfd3e776eddddf20afafaa99
Renata Martins Marchese <renatapink@users.noreply.github.com>
2021-07-15 11:29:19 -0300

    Herança e Bibliotecas

---
+++ Aula16/contas.py
@@ -0,0 +1,13 @@
+def soma(a, b):
+    resultado = a + b
+    return resultado
+
+def media(x, y):
+    s = soma(x, y)
+    resultado = s/2
+    return resultado
+
+def subtracao(a, b):
+    return a - b
+
+var = 5
